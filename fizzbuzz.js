a31fe93d3df0f6ac667a7f43a1704062544727ed
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-20 08:33:26 -0700

    Two solutions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,16 @@
 function fizz1() {
   for (i = 0; i < 100; ) {
-    console.log(((++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz")) | i);
+    console.log((++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i);
   }
-  return;
 }
 
 function fizz2() {
   for (i = 0; i < 100; ) {
     console.log(
-      ((++i % 3 === 0 ? "Fizz" : "") + (i % 5 === 0 ? "Buzz" : "")) | i
+      (++i % 3 === 0 ? "Fizz" : "") + (i % 5 === 0 ? "Buzz" : "") || i
     );
   }
   return;
 }
 
-fizz1();
+fizz2();
